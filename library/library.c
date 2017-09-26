f06378f224e180577b5424b4d51fddf1c62d289b
Liam Ryan <liamryandev@gmail.com>
2017-09-26 18:16:58 +0100

    Add mergesort (currently broken)

--- library/library.c
+++ library/library.c
@@ -1,6 +1,8 @@
-#include "library.h"
+#include <math.h>
 
-int *lrsortal_insertion_sort(int *array, int size)
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+void lrsortal_insertion_sort(int *array, int size)
 {
 	for (int i = 1; i < size; i++) {
 		int j = i - 1;
@@ -10,5 +12,49 @@ int *lrsortal_insertion_sort(int *array, int size)
 			j--;
 		}
 		array[j + 1] = sort_val;
+	}
+}
+
+void merge(int *array, int strt, int mid, int end)
+{
+	int lowSize = mid - strt + 1;
+	int highSize = end - mid;
+	int lowArr[lowSize];
+	int highArr[highSize];
+	for (int i = strt; i <= end; i++) {
+		if (i <= mid)
+			lowArr[i] = array[i];
+		else
+			highArr[i - mid] = array[i];
+	}
+
+	int i = 0;
+	int j = 0;
+	while( i < lowSize && j < highSize) {
+		int index = i + j;
+		array[index] = min(lowArr[i], highArr[j]);
+		if (array[index] == lowArr[i])
+			i++;
+		else
+			j++;
+	}
+	if( i < lowSize) {
+		while( i < lowSize )
+			array[j + i] = lowArr[i++];
+	} else {
+		while (j < highSize) {
+			array[j + i] = highArr[j++];
+		}
+	}
+
+}
+
+void lrsortal_merge_sort(int *array, int strt, int end)
+{
+	if (end > strt) {
+		int mid = floor((end - strt) / 2) + strt;
+		lrsortal_merge_sort(array, strt, mid);
+		lrsortal_merge_sort(array, mid + 1, end);
+		merge(array, strt, mid, end);
 	}
 }
--- library/library.h
+++ library/library.h
@@ -1,6 +1,8 @@
 #ifndef LR_SORTING_ALGORITHMS_H
 #define LR_SORTING_ALGORITHMS_H
 
-int *lrsortal_insertion_sort(int *array, int length);
+void lrsortal_insertion_sort(int *array, int length);
+
+void lrsortal_merge_sort(int *array, int strt, int end);
 
 #endif
