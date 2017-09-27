0326e23cf970f7ffb5ece123cb35e61c31af865d
Liam Ryan <liamryandev@gmail.com>
2017-09-27 17:50:13 +0100

    Fixed mergesort

--- library/library.c
+++ library/library.c
@@ -17,33 +17,37 @@ void lrsortal_insertion_sort(int *array, int size)
 
 void merge(int *array, int strt, int mid, int end)
 {
+	//temp arrays for lower and higher split of array
 	int lowSize = mid - strt + 1;
 	int highSize = end - mid;
 	int lowArr[lowSize];
 	int highArr[highSize];
-	for (int i = strt; i <= end; i++) {
-		if (i <= mid)
-			lowArr[i] = array[i];
-		else
-			highArr[i - mid] = array[i];
-	}
 
+	for (int i = strt; i <= mid; i++)
+		lowArr[i - strt] = array[i];
+	for (int i = mid + 1; i <= end; i++)
+		highArr[i - (mid + 1)] = array[i];
+
+	//indices to crawl the lower and higher temp arrays
 	int i = 0;
 	int j = 0;
-	while( i < lowSize && j < highSize) {
-		int index = i + j;
+	int index = i + j + strt;
+
+	while (i < lowSize && j < highSize) {
 		array[index] = min(lowArr[i], highArr[j]);
 		if (array[index] == lowArr[i])
 			i++;
 		else
 			j++;
+		index++;
 	}
-	if( i < lowSize) {
-		while( i < lowSize )
-			array[j + i] = lowArr[i++];
+	//finish adding the rest of the array that still has sorted elements in it
+	if (i < lowSize) {
+		while (i < lowSize)
+			array[index++] = lowArr[i++];
 	} else {
 		while (j < highSize) {
-			array[j + i] = highArr[j++];
+			array[index++] = highArr[j++];
 		}
 	}
 
@@ -52,7 +56,7 @@ void merge(int *array, int strt, int mid, int end)
 void lrsortal_merge_sort(int *array, int strt, int end)
 {
 	if (end > strt) {
-		int mid = floor((end - strt) / 2) + strt;
+		int mid = (int)floor((end - strt) / 2) + strt;
 		lrsortal_merge_sort(array, strt, mid);
 		lrsortal_merge_sort(array, mid + 1, end);
 		merge(array, strt, mid, end);
